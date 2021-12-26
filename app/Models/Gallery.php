da84ba2312313e0027ce5ee5c7a9937a214e7ab9
SQdev-git <sqmailer@gmail.com>
2021-12-26 19:08:49 +0500

    MSF-8 done, created models in app/Models and migration for table Gallery, GalleryCategory

---
+++ app/Models/Gallery.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Gallery extends Model
+{
+    use HasFactory;
+
+    const TYPE_IMAGE = 0;
+
+    const TYPE_VIDEO = 1;
+}
---
+++ database/migrations/2021_12_26_135818_create_galeries_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGaleriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('galleries', function (Blueprint $table) {
+            $table->id();
+            $table->tinyInteger('type')->unsigned();
+            $table->bigInteger('category_id')->unsigned();
+            $table->string('path', 512);
+            $table->timestamps();
+
+            $table->foreign('category_id')->references('id')->on('gallery_categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('galeries');
+    }
+}
