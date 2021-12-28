228b2795312bdb49e07793b56969563c32d0f51e
husanjan <husanjan2019@gmail.com>
2021-12-28 20:50:27 +0300

    MSP-7

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,77 +1,65 @@
-@extends('layouts.app')
+@extends('layouts.frontend.register')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
 
-                        <div class="row mb-3">
-                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
 
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+    <form  class="woocommerce-form woocommerce-form-login login" method="post">
+        @csrf
 
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
 
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('E-Mail Address') }}</label>
+        <p class="woocommerce-form-row form-row  ms-form-control material">
+            <label for="reg_email">Email address <span
+                        class="required">*</span></label>
+            <input type="email" class="woocommerce-Input woocommerce-Input--text input-text form-control" name="email"
+                   id="reg_email" autocomplete="email"
+                   value=""/>    </p>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
+        <p class="woocommerce-form-row form-row  ms-form-control material">
+            <label for="reg_password">Password <span
+                        class="required">*</span></label>
+            <input type="password" class="woocommerce-Input woocommerce-Input--text input-text form-control"
+                   name="password" id="reg_password" autocomplete="new-password"/>
 
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+        </p>
+        <p class="woocommerce-form-row form-row  ms-form-control material">
+            <label for="reg_password">Confirm <span
+                        class="required">*</span></label>
+            <input type="password" name="password_confirmation"  class="woocommerce-Input woocommerce-Input--text input-text form-control"
+                   name="password" id="reg_password" autocomplete="new-password"/>
 
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
 
-                        <div class="row mb-3">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
+        </p>
 
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
 
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
+
+        <div class="woocommerce-privacy-policy-text"><p>Your personal data will be used to support your experience throughout this website, to manage access to your account, and for other purposes described in our <a href="https://www.mrstairs.com/privacy-policy/" class="woocommerce-privacy-policy-link" target="_blank">privacy policy</a>.</p>
+        </div><div class="anr_captcha_field"><div id="anr_captcha_field_1" class="anr_captcha_field_div"><input type="hidden" name="g-recaptcha-response" value=""/></div></div>
+        <p class="d-block clearfix mt-2 mb-4">
+            <a class="d-inline-block"
+               href="https://www.mrstairs.com">Already have an account?</a>
+        </p>
+
+        <div class="actions">
+            <input type="hidden" id="woocommerce-register-nonce" name="woocommerce-register-nonce" value="77a50cc7bf" /><input type="hidden" name="_wp_http_referer" value="/create-an-account/" />        <button type="submit" class="btn-lg float-left btn btn-primary" name="register"
+                                                                                                                                                                                                                   value="Log in">Register</button>
+
         </div>
-    </div>
-</div>
+
+
+    </form>
+
+
+
+
+
+
+
+
+
+
+
+
 @endsection
