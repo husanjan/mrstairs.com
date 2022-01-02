ff40be4a85e47be2109d075bcbea063f73739856
Akmal <akmaljon.gafforov@gmail.com>
2022-01-02 15:21:15 +0500

    - Add gitignore for public css/js folders.

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,11 +14,3 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
     .css('resources/css/app.css', 'public/css')
     .css('resources/css/login.css', 'public/css');
-
-mix.js('resources/js/Frontend/Auth/Login/old-app.js', 'public/js');
-//mix.postCss('resources/css/Frontend/Auth/Login/old-login.css', 'public/css');
-mix.postCss('resources/css/Frontend/Auth/Lost/base.min.css', 'public/css');
-mix.postCss('resources/css/Frontend/Auth/Lost/widget-nav.min.css', 'public/css');
-mix.postCss('resources/css/Frontend/Auth/Lost/widget-layered-nav-stock-status.min.css', 'public/css');
-mix.postCss('resources/css/Frontend/Auth/Lost/woo-widget-active-filters.min.css', 'public/css');
-mix.postCss('resources/css/Frontend/Auth/Lost/footer-base.min.css', 'public/css');
