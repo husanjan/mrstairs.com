9f8f5ce6596a9b7a1073df4637c8d6c9ca8b2982
Akmal <akmaljon.gafforov@gmail.com>
2022-01-09 17:34:04 +0500

    - Fixed GallerySeeder.php

--- database/seeders/GallerySeeder.php
+++ database/seeders/GallerySeeder.php
@@ -6,7 +6,6 @@ use App\Models\GalleryItem;
 use Illuminate\Database\Seeder;
 use App\Models\GalleryItemCategory;
 use App\Models\LinkGalleryCategory;
-use Illuminate\Support\Facades\DB;
 
 /**
  * Class GallerySeeder
@@ -15,6 +14,19 @@ use Illuminate\Support\Facades\DB;
  */
 class GallerySeeder extends Seeder
 {
+    public $images = [
+        'Oak' => [
+            "https://www.mrstairs.com/wp-content/uploads/2018/12/2018-02-25-10.08.08-min.jpg",
+        ],
+        'Cut string' => [
+            "https://www.mrstairs.com/wp-content/uploads/2018/12/Pars-Joinery-Staircase-min.jpg",
+        ],
+        'Pine' => [],
+        'Quarter turn' => [],
+        'Straight staircase' => [],
+        'Half turn' => [],
+    ];
+
     /**
      * Run the database seeds.
      *
@@ -22,22 +34,41 @@ class GallerySeeder extends Seeder
      */
     public function run()
     {
-        // GalleryItemCategory::factory(5)->create();
-        // GalleryItem::factory(20)->create();
-        // LinkGalleryCategory::factory(20)->create();
+        foreach ($this->images as $key => $images) {
+            $category = GalleryItemCategory::where('name', $key)->first();
+            if (empty($category)) {
+                $category = GalleryItemCategory::create(['name' => $key]);
+            }
 
-        $this->createCategory();
-    }
+            foreach ($images as $image) {
+                $galleryItem = GalleryItem::where('type', GalleryItem::TYPE_IMAGE)
+                    ->where('path', $image)
+                    ->first();
 
-    public function createCategory()
-    {
-        $datas = ['oak', 'cut string', 'pine', 'Quarter turn', 'Half turn'];
-        
-        foreach ($datas as $data) {
-            GalleryItemCategory::create([
-                'name' => $data
-            ]);
+                if (empty($galleryItem)) {
+                    $galleryItem = GalleryItem::create([
+                        'type' => GalleryItem::TYPE_IMAGE,
+                        'path' => $image,
+                    ]);
+                }
+
+                if (empty($category) || empty($galleryItem)) {
+                    continue;
+                }
+
+                $link = LinkGalleryCategory::where('gallery_item_id', $galleryItem->id)
+                    ->where('gallery_item_category_id', $category->id)
+                    ->first();
+
+                if (!empty($link)) {
+                    continue;
+                }
+
+                LinkGalleryCategory::create([
+                    'gallery_item_id' => $galleryItem->id,
+                    'gallery_item_category_id' => $category->id,
+                ]);
+            }
         }
-        
     }
 }
