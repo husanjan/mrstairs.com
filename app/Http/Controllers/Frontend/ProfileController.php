6a613ebc505c0b02ca23fb26ce29e03bcbd651f1
SQdev-git <sqmailer@gmail.com>
2022-01-24 14:07:26 +0500

    added feture change account

--- app/Http/Controllers/Frontend/ProfileController.php
+++ app/Http/Controllers/Frontend/ProfileController.php
@@ -3,6 +3,11 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Frontend\ProfileRequest;
+use App\Models\User;
+use App\Rules\Frontend\ProfileRule;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 /**
  * Class ProfileController
@@ -16,11 +21,30 @@ class ProfileController extends Controller
      */
     public function show()
     {
-        return view('frontend.profile.show');
+        $user = User::find(auth()->id());
+        return view('frontend.profile.show', compact('user'));
     }
 
-    public function edit()
+    public function edit(ProfileRequest $request)
     {
-        // Инҷаба формая данныхоша сохранить кунед
+        $user = User::find(auth()->user()->id);
+
+        if ($request->current_password) {
+            $request->validate([
+                'current_password' => ['required', new ProfileRule],
+                'password_1' => 'required|min:8',
+                'password_2' => 'same:password_1'
+            ]);
+
+            $user->update([
+                'password' => Hash::make($request->password_1),
+                'first_name' => $request->first_name,
+                'last_name' => $request->last_name,
+                'name' => $request->name,
+                'email' => $request->email,
+            ]);
+        }else {
+            $user->update($request->validated());
+        }
     }
 }
--- resources/views/frontend/profile/show.blade.php
+++ resources/views/frontend/profile/show.blade.php
@@ -1,103 +1,133 @@
 @extends('layouts.frontend.app')
 
 @section('styles')
-    <link href="{{ asset('css/profile.css') }}" rel="stylesheet">
+<link href="{{ asset('css/profile.css') }}" rel="stylesheet">
 @endsection
 
 @section('title', 'My Account')
 
 @section('content')
-    <div class="site-content col-lg-12 col-12 col-md-12" role="main">
-
-        <article id="post-20" class="post-20 page type-page status-publish hentry">
-
-            <div class="entry-content">
-                <div class="woocommerce">
-                    <div class="woocommerce-my-account-wrapper">
-                        <div class="wd-my-account-sidebar woodmart-my-account-sidebar">
-                            <h3 class="woocommerce-MyAccount-title entry-title">
-                                My account </h3>
-
-                            <nav class="woocommerce-MyAccount-navigation" role="navigation">
-                                <div class="list-group">
-                                    <a href="https://www.mrstairs.com/my-account/" class="list-group-item list-group-item-action">Dashboard</a>
-                                    <a href="https://www.mrstairs.com/my-account/orders/" class="list-group-item list-group-item-action">Orders</a>
-                                    <a href="https://www.mrstairs.com/my-account/downloads/" class="list-group-item list-group-item-action">Downloads</a>
-                                    <a href="https://www.mrstairs.com/my-account/edit-address/" class="list-group-item list-group-item-action">Addresses</a>
-                                    <a href="https://www.mrstairs.com/my-account/edit-account/" class="list-group-item list-group-item-action">Account
-                                        details</a>
-                                    <a href="https://www.mrstairs.com/my-account/customer-logout/?_wpnonce=08f22c7623" class="list-group-item list-group-item-action">Logout</a>
-                                </div>
-                            </nav>
-
-                        </div><!-- .wd-my-account-sidebar -->
-                        <div class="woocommerce-MyAccount-content">
-                            <div class="woocommerce-notices-wrapper"></div>
-                            <form class="woocommerce-EditAccountForm edit-account" action="" method="post">
-
-
-                                <p class="woocommerce-form-row woocommerce-form-row--first form-row form-row-first">
-                                    <label for="account_first_name">First
-                                        name&nbsp;<span class="required">*</span></label>
-                                    <input type="text" class="woocommerce-Input woocommerce-Input--text input-text" name="account_first_name" id="account_first_name" autocomplete="given-name" value="Akmal">
-                                </p>
-                                <p class="woocommerce-form-row woocommerce-form-row--last form-row form-row-last">
-                                    <label for="account_last_name">Last
-                                        name&nbsp;<span class="required">*</span></label>
-                                    <input type="text" class="woocommerce-Input woocommerce-Input--text input-text" name="account_last_name" id="account_last_name" autocomplete="family-name" value="Gafforov">
-                                </p>
-                                <div class="clear"></div>
-
+<div class="site-content col-lg-12 col-12 col-md-12" role="main">
+    <article id="post-20" class="post-20 page type-page status-publish hentry">
+        <div class="entry-content">
+            <div class="woocommerce">
+                <div class="woocommerce-my-account-wrapper">
+                    <div class="wd-my-account-sidebar woodmart-my-account-sidebar">
+                        <h3 class="woocommerce-MyAccount-title entry-title">
+                            My account
+                        </h3>
+                        <nav class="woocommerce-MyAccount-navigation" role="navigation">
+                            <div class="list-group"> <a href="https://www.mrstairs.com/my-account/"
+                                    class="list-group-item list-group-item-action">Dashboard</a> <a
+                                    href="https://www.mrstairs.com/my-account/orders/"
+                                    class="list-group-item list-group-item-action">Orders</a> <a
+                                    href="https://www.mrstairs.com/my-account/downloads/"
+                                    class="list-group-item list-group-item-action">Downloads</a> <a
+                                    href="https://www.mrstairs.com/my-account/edit-address/"
+                                    class="list-group-item list-group-item-action">Addresses</a> <a
+                                    href="https://www.mrstairs.com/my-account/edit-account/"
+                                    class="list-group-item list-group-item-action">Account details</a> <a
+                                    href="https://www.mrstairs.com/my-account/customer-logout/?_wpnonce=08f22c7623"
+                                    class="list-group-item list-group-item-action">Logout</a> </div>
+                        </nav>
+                    </div>
+                    <!-- .wd-my-account-sidebar -->
+                    <div class="woocommerce-MyAccount-content">
+                        <div class="woocommerce-notices-wrapper"></div>
+                        <form class="woocommerce-EditAccountForm edit-account" action="" method="post">
+                            <p class="woocommerce-form-row woocommerce-form-row--first form-row form-row-first">
+                                <label for="account_first_name">First name&nbsp;<span class="required">*</span></label>
+                                <input 
+                                    type="text" 
+                                    class="woocommerce-Input woocommerce-Input--text input-text"
+                                    name="first_name" 
+                                    id="account_first_name" 
+                                    autocomplete="given-name"
+                                    value="{{ $user->first_name }}" />
+                            </p>
+                            <p class="woocommerce-form-row woocommerce-form-row--last form-row form-row-last">
+                                <label for="account_last_name">Last name&nbsp;<span class="required">*</span></label>
+                                <input 
+                                    type="text" 
+                                    class="woocommerce-Input woocommerce-Input--text input-text"
+                                    name="last_name" id="account_last_name" 
+                                    autocomplete="family-name"
+                                    value="{{ $user->last_name }}" />
+                            </p>
+                            <div class="clear"></div>
+                            <p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
+                                <label for="name">Display name&nbsp;<span
+                                        class="required">*</span></label>
+                                <input 
+                                    type="text" 
+                                    class="woocommerce-Input woocommerce-Input--text input-text"
+                                    name="account_display_name" 
+                                    id="account_display_name" 
+                                    value="{{ $user->name }}" />
+                                <span><em>This will be how your name will be displayed in the account section and in
+                                        reviews</em></span>
+                            </p>
+                            <div class="clear"></div>
+                            <p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
+                                <label for="account_email">Email address&nbsp;<span class="required">*</span></label>
+                                <input 
+                                    type="email" 
+                                    class="woocommerce-Input woocommerce-Input--email input-text"
+                                    name="account_email" 
+                                    id="account_email" 
+                                    autocomplete="email"
+                                    value="{{ $user->email }}" 
+                                    required/>
+                            </p>
+                            <fieldset>
+                                <legend>Password change</legend>
                                 <p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
-                                    <label for="account_display_name">Display name&nbsp;<span class="required">*</span></label>
-                                    <input type="text" class="woocommerce-Input woocommerce-Input--text input-text" name="account_display_name" id="account_display_name" value="Akmal">
-                                    <span><em>This will be how your name will be displayed in the account section and in reviews</em></span>
+                                    <label for="password_current">Current password (leave blank to leave
+                                        unchanged)</label> <span class="password-input">
+                                        <input 
+                                            type="password"
+                                            class="woocommerce-Input woocommerce-Input--password input-text"
+                                            name="password_current" 
+                                            id="password_current" 
+                                            autocomplete="off" />
+                                            <span class="show-password-input"></span> </span>
                                 </p>
-                                <div class="clear"></div>
-
                                 <p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
-                                    <label for="account_email">Email
-                                        address&nbsp;<span class="required">*</span></label>
-                                    <input type="email" class="woocommerce-Input woocommerce-Input--email input-text" name="account_email" id="account_email" autocomplete="email" value="akmal.ghafforov@gmail.com">
+                                    <label for="password_1">New password (leave blank to leave unchanged)</label> <span
+                                        class="password-input">
+                                        <input type="password"
+                                            class="woocommerce-Input woocommerce-Input--password input-text"
+                                            name="password_1" 
+                                            id="password_1" 
+                                            autocomplete="off" /><span
+                                            class="show-password-input"></span> </span>
                                 </p>
-
-                                <fieldset>
-                                    <legend>Password change</legend>
-
-                                    <p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
-                                        <label for="password_current">Current password (leave blank to leave
-                                            unchanged)</label>
-                                        <span class="password-input"><input type="password" class="woocommerce-Input woocommerce-Input--password input-text" name="password_current" id="password_current" autocomplete="off"><span class="show-password-input"></span></span>
-                                    </p>
-                                    <p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
-                                        <label for="password_1">New password (leave blank to leave unchanged)</label>
-                                        <span class="password-input"><input type="password" class="woocommerce-Input woocommerce-Input--password input-text" name="password_1" id="password_1" autocomplete="off"><span class="show-password-input"></span></span>
-                                    </p>
-                                    <p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
-                                        <label for="password_2">Confirm new password</label>
-                                        <span class="password-input"><input type="password" class="woocommerce-Input woocommerce-Input--password input-text" name="password_2" id="password_2" autocomplete="off"><span class="show-password-input"></span></span>
-                                    </p>
-                                </fieldset>
-                                <div class="clear"></div>
-
-
-                                <p>
-                                    <input type="hidden" id="save-account-details-nonce" name="save-account-details-nonce" value="958e45f281"><input type="hidden" name="_wp_http_referer" value="/my-account/edit-account/">
-                                    <button type="submit" class="woocommerce-Button button" name="save_account_details" value="Save changes">
-                                        Save changes
-                                    </button>
-                                    <input type="hidden" name="action" value="save_account_details">
+                                <p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
+                                    <label for="password_2">Confirm new password</label> 
+                                    <span class="password-input">
+                                        <input type="password"
+                                            class="woocommerce-Input woocommerce-Input--password input-text"
+                                            name="password_2"
+                                            id="password_2" 
+                                            autocomplete="off" />
+                                            <span class="show-password-input"></span> 
+                                    </span>
                                 </p>
-
-                            </form>
-
-                        </div><!-- .woocommerce-my-account-wrapper --></div>
+                            </fieldset>
+                            <div class="clear"></div>
+                            <p>  
+                                <button type="submit" 
+                                    class="woocommerce-Button button" 
+                                    name="save_account_details"
+                                    value="Save changes"> Save changes </button>
+                            </p>
+                        </form>
+                    </div>
+                    <!-- .woocommerce-my-account-wrapper -->
                 </div>
             </div>
-
-
-        </article><!-- #post -->
-
-
-    </div>
+        </div>
+    </article>
+    <!-- #post -->
+</div>
 @endsection
